0c39922d5c3195f10bca821a82fe02517fdf2a43
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-11-21 17:51:41 -0500

    added basic html structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,25 @@
-var TestObject = Parse.Object.extend("TestObject");
-var testObject = new TestObject();
-testObject.save({foo: "bar"}, {
-  success: function(object) {
-    alert("yay! it worked");
-  }
-});
+var days = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su'];
+
+var hours = ['8', '9', '10', '11', '12', '1', '2', '3', '4', '5']
+var increments = ['15', '30', '45', '00']
+
+var dayTemplate = _.template($('#days').text())
+var incrementTemplate = _.template($('#slots').text())
+
+
+$(document).ready(function() {
+
+	days.forEach(function(day) {
+		$('.schedule').append(dayTemplate({day: day}))
+	})
+
+	hours.forEach(function(hour) {
+		increments.forEach(function(increment) {
+			$('.slots').append(incrementTemplate({options: {
+				hour: hour,
+				increment: increment
+			}}))
+		})
+	})
+	
+})
