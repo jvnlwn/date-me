6db4c07861c908add70f99c16b530daca9f5f413
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-11-21 14:08:44 -0500

    first commit and  parse hooked up

---
+++ app/scripts/main.js
@@ -0,0 +1,7 @@
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}, {
+  success: function(object) {
+    alert("yay! it worked");
+  }
+});
---
+++ app/scripts/parse.js
@@ -0,0 +1,1 @@
+Parse.initialize("nBtaWx3Z6qemHuUwn5x1aMalAyTGwopll122Ojvy", "XWSe7WOWNFUTJnpzKNywK1ONDysUwjbvS9MNZUfW");
