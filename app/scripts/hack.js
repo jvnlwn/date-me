b2e4a1002361a9524b51864da31cadb8601715bb
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-11-22 15:20:40 -0500

    changed file name

--- app/scripts/hack.js
+++ app/scripts/hack.js
@@ -1,7 +1,7 @@
 var days = ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su'];
 
 var hours = ['8', '9', '10', '11', '12', '1', '2', '3', '4', '5']
-var increments = ['15', '30', '45', '00']
+var increments = ['00', '15', '30', '45']
 
 var dayTemplate = _.template($('#days').text())
 var incrementTemplate = _.template($('#slots').text())
