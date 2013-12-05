2cf66b3d3984c8d27ffbce4d59992d38459d6f54
Joe VanLeeuwen <jvnlwn@gmail.com>
2013-12-05 10:23:40 -0500

    got correct months with days in month and year

--- app/scripts/hack.js
+++ app/scripts/hack.js
@@ -1,68 +1,101 @@
-var dayTemplate = _.template($('#days').text())
+var setMonths = monthSetter(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
 
-var weekTracker = 0;
+daysInMonth(setMonths(moment().format('MMMM')))
 
-$(document).ready(function() {
-
-	displayWeeks(0)
-
-	upDown('up');
-	upDown('down');
-	
-})
-
-function upDown(direction) {
-	$('.' + direction).click(function() {
-
-		if (direction === 'up') {
-
-			var firstDay = $('.schedule').children().first().children().first().children().last().text();
-			var today = moment().format('MMM') + ' ' + moment().format('D');
-
-			if (firstDay !== today) {
-				
-				weekTracker -= 1;
-
-				$('.schedule').html('');
-
-				displayWeeks(Math.abs(weekTracker * 14))
-
-			}
-
-		} else {
-				weekTracker += 1;
-
-				$('.schedule').html('');
-
-				displayWeeks(Math.abs(weekTracker * 14))
-
-		}
-			$('.wrapper').append('<div class="cf"></div>')
-	})
-}
-
-
-function displayWeeks(start) {
-
-	for (i = start; i < (start + 14); i++) {
-
-		var options = {
-			day:   moment().day(moment().day() + i).format('dd'),
-			month: moment().day(moment().day() + i).format('MMM'),
-			date:  moment().day(moment().day() + i).format('D')
-		}
-
-		$('.schedule').append(dayTemplate({options: options}))
-
-		if (($('.weekday').length === 7 || $('.weekday').length === 14) && i !== 0) {
-			$('.schedule').children().last().css('margin-right', '0')
-		}
-
-		$('.wrapper').append('<div class="cf"></div>')
-
+function monthSetter(months) {
+	return function(startingMonth) {
+		return months.slice(months.indexOf(startingMonth)).concat(months.slice(0, months.indexOf(startingMonth)))	
 	}
 }
 
+function daysInMonth(months) {
+	var year = determineYear(moment().get('years'))
 
+	var months = months.slice()
+	var days = [
+		{
+			month: 'January',
+			days: 31,
+		},
+		{
+			month: 'February',
+			days: isLeapYear(months),
+		},
+		{
+			month: 'March',
+			days: 31,
+		},
+		{
+			month: 'April',
+			days: 30,
+		},
+		{
+			month: 'May',
+			days: 31,
+		},
+		{
+			month: 'June',
+			days: 30,
+		},
+		{
+			month: 'July',
+			days: 31,
+		},
+		{
+			month: 'August',
+			days: 31,
+		},
+		{
+			month: 'September',
+			days: 30,
+		},
+		{
+			month: 'October',
+			days: 31,
+		},
+		{
+			month: 'November',
+			days: 30,
+		},
+		{
+			month: 'December',
+			days: 31,
+		}
+	]
 
+	var newDays = []
 
+	months.forEach(function(month) {
+		newDays.push(_.find(days, function(day) {
+			return day.month === month
+		}))
+
+		newDays[newDays.length - 1].year = year(month)
+	})
+
+	console.log(newDays)
+}
+
+function determineYear(year) {
+
+	return function(month) {
+
+		if (month === 'December') {
+			year = moment().add('years', 1).format('YYYY');
+			return moment().get('years')
+		}
+		return parseInt(year)
+	}
+}
+
+function isLeapYear(months) {
+
+	if (months.indexOf('February') < months.indexOf('December')) {
+		return moment().isLeapYear() ? 29 : 28;	
+	} else {
+		return moment().add('years', 1).isLeapYear() ? 29 : 28
+	}
+}
+
+// get today's date
+// moment().date()
